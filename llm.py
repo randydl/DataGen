b48a442b848c64d3ae8faaa88cb2fd67b98b3bf6
Hui Kang <hui.kang@cxmt.com>
2025-04-18 20:22:28 +0800

    temp

--- llm.py
+++ llm.py
@@ -4,7 +4,7 @@ from langchain_core.documents import Document
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from utils import split_text
-from prompts import PROMPT, REFINE_PROMPT
+from prompts import PROMPT, REFINE_PROMPT, CONDENSE_PROMPT
 
 
 def create_model(**kwargs):
@@ -33,7 +33,10 @@ class LLMChain:
     def summarize(self, texts):
         documents = [Document(x) for x in texts]
         summary = self.refine.invoke(documents)
-        return summary.get('output_text')
+        prompt_template  = PromptTemplate.from_template(CONDENSE_PROMPT)
+        prompt_template = prompt_template.invoke({'structured_summary': summary.get('output_text')})
+
+        return self.invoke(prompt_template.text)
 
     def split_text(self, *args, **kwargs):
         return split_text(*args, **kwargs)
@@ -47,10 +50,10 @@ if __name__ == '__main__':
         base_url='http://10.252.36.121:18000/v1'
     )
 
-    result = llm.invoke('DRAM的主要功能是什么？')
+    # result = llm.invoke('DRAM的主要功能是什么？')
 
     text = Path('example.md').read_text('utf-8')
-    texts = llm.split_text(text, chunk_size=5000)
-    result = llm.summarize(texts)
+    texts = llm.split_text(text, chunk_size=1000)
+    result = llm.summarize(texts[:2])
 
     print(result)
